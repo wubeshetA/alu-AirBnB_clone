760ac311643ff24ef651847297e982822f2a174e
wubeshetA <w.yimam@alustudent.com>
2023-01-21 08:37:30 +0300

    Accept Key Word arguments on the BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,32 +4,41 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """Base class for all common attributes and methods"""
 
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+
+        # if keyword argument is provided initialize class with the specified
+        # values
+        if kwargs != {}:
+            for key, val in kwargs.items():
+                if key == '__class__':
+                    continue
+                self.__setattr__(self, key, val)
+            datetime.fromisoformat(self.__getattribute__('created_at'))
+            datetime.fromisoformat(self.__getattribute__('updated_at'))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return a string representation of BaseModel"""
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """Update the updated_at attribute"""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """Return a dict representation of the BaseModel"""
-        
+
         formated_dict = self.__dict__.copy()
         formated_dict['created_at'] = formated_dict['created_at'].isoformat()
         formated_dict['updated_at'] = formated_dict['updated_at'].isoformat()
         formated_dict['__class__'] = self.__class__.__name__
         return formated_dict
-
-
-        
