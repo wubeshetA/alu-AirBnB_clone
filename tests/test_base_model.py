479f6ddea4f320e2616f960dabaaf16d816b2e12
wubeshetA <w.yimam@alustudent.com>
2023-01-21 05:38:46 +0300

    Change module import method

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,10 +1,11 @@
 import unittest
 import sys
 import time
+import os
 
-sys.path.append('/home/wubeshet/alu/alu-AirBnB_clone')
-
+sys.path.append(os.getcwd())
 from models.base_model import BaseModel
+
 class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel class."""
 
@@ -12,12 +13,13 @@ class TestBaseModel(unittest.TestCase):
         self.myModel = BaseModel()
 
     def test_save(self):
-        # test it before and after save()
+        # test BaseModel before and after clling save method
         updated_at_before_save = self.myModel.updated_at
         time.sleep(0.5)
         self.myModel.save()
         updated_at_after_save = self.myModel.updated_at
         self.assertNotEqual(updated_at_before_save, updated_at_after_save)
+
 
 if __name__ == "__main__":
     unittest.main()
