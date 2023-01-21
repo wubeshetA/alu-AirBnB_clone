5101b06f50e6b64834fe99e66a5b975a894f78a3
wubeshetA <w.yimam@alustudent.com>
2023-01-21 06:02:22 +0300

    Add tests for BaseModel.to_dict method

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 import sys
 import time
@@ -10,16 +11,31 @@ class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel class."""
 
     def setUp(self):
-        self.myModel = BaseModel()
+        self.testModel = BaseModel()
 
+    # test BaseModel before and after clling save method
     def test_save(self):
-        # test BaseModel before and after clling save method
-        updated_at_before_save = self.myModel.updated_at
+        updated_at_before_save = self.testModel.updated_at
         time.sleep(0.5)
-        self.myModel.save()
-        updated_at_after_save = self.myModel.updated_at
+        self.testModel.save()
+        updated_at_after_save = self.testModel.updated_at
         self.assertNotEqual(updated_at_before_save, updated_at_after_save)
 
-
+    # test BaseModel to_dict method return type
+    def test_to_dict_return(self):
+        testModel_dict = self.testModel.to_dict()
+        self.assertIsInstance(testModel_dict, dict)
+    
+    # test BaseModel to_dict method for it's content.
+    def test_to_dict_value(self):
+        testModel_dict = self.testModel.to_dict()
+        self.assertIn('__class__', testModel_dict)
+    
+    # test BaseModel to_dict method if it's content has the correct types
+    def test_to_dict_content_type(self):
+        testModel_dict = self.testModel.to_dict()
+        self.assertIsInstance(testModel_dict.get('created_at'), str)
+        self.assertIsInstance(testModel_dict.get('created_at'), str)
+        
 if __name__ == "__main__":
     unittest.main()
