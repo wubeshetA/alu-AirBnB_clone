4495b6c58be6c8f7ae355decec30613f671db73c
wubeshetA <w.yimam@alustudent.com>
2023-01-24 21:11:06 +0300

    Update BaseModel tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 import time
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel class."""
 
@@ -21,24 +22,26 @@ class TestBaseModel(unittest.TestCase):
     def test_to_dict_return(self):
         testModel_dict = self.testModel.to_dict()
         self.assertIsInstance(testModel_dict, dict)
-    
+
     # test BaseModel to_dict method for it's content.
     def test_to_dict_value(self):
         testModel_dict = self.testModel.to_dict()
         self.assertIn('__class__', testModel_dict)
-    
+
     # test BaseModel to_dict method if it's content has the correct types
     def test_to_dict_content_type(self):
         testModel_dict = self.testModel.to_dict()
         self.assertIsInstance(testModel_dict.get('created_at'), str)
         self.assertIsInstance(testModel_dict.get('created_at'), str)
-    
+
     # test string value of BaseModel
     def test__str__(self):
         class_name = self.testModel.__class__.__name__
         id = self.testModel.id
         dict_v = self.testModel.__dict__
-        self.assertEqual(str(self.testModel), f"[{class_name}] ({id}) {dict_v}")
-        
+        self.assertEqual(str(self.testModel),
+                         f"[{class_name}] ({id}) {dict_v}")
+
+
 if __name__ == "__main__":
     unittest.main()
