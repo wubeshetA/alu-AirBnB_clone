87cc9af18e92bf002a915e6eeb72e2650d8e62ad
wubeshetA <w.yimam@alustudent.com>
2023-01-24 23:40:40 +0300

    Add Review module and it's tests

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""user module for the AirBnB clone"""
+
+from models import BaseModel
+
+
+class Review(BaseModel):
+    """Review class"""
+
+    place_id = "" # string - empty string: it will be the Place.id
+    user_id = "" # string - empty string: it will be the User.id
+    text = "" # string - empty string
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""unittest for User class"""
+import unittest
+from models.review import Review
+from models import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    """Test cases for User class."""
+
+    def setUp(self):
+        self.testReview = Review()
+
+    def testState(self):
+        self.assertTrue(issubclass(self.testReview.__class__, BaseModel))
+
+    def test_place_id(self):
+        self.assertIsInstance(self.testReview.place_id, str)
+    
+    def test_user_id(self):
+        self.assertIsInstance(self.testReview.user_id, str)
+    
+    def test_text(self):
+        self.assertIsInstance(self.testReview.text, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
