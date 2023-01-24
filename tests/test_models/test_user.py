830c49caa5567ac3bb694a99b8fc5da72544559d
wubeshetA <w.yimam@alustudent.com>
2023-01-24 21:38:53 +0300

    Add tests for User class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""unittest for User class"""
+import unittest
+from models.user import User
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for User class."""
+
+    def setUp(self):
+        self.testUser = User()
+
+    def test_email(self):
+        self.assertIsInstance(self.testUser.email, str)
+
+    def test_password(self):
+        self.assertIsInstance(self.testUser.password, str)
+
+    def test_first_name(self):
+        self.assertIsInstance(self.testUser.first_name, str)
+
+    def test_last_name(self):
+        self.assertIsInstance(self.testUser.last_name, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
