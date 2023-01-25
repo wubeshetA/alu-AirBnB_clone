d7507b382ff15c90f29fda15bdce001df61536d4
wubeshetA <w.yimam@alustudent.com>
2023-01-25 09:15:25 +0300

    Import all model classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,11 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
