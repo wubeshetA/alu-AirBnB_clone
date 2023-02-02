b7fdb162b2cb0eb7a6440792eecd2c8a0b1e84fd
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-02 18:45:48 +0300

    Add tests for show command

--- tests/test_console.py
+++ tests/test_console.py
@@ -36,6 +36,27 @@ class TestConsole(unittest.TestCase):
             "-[0-9a-f]{4}-[0-9a-f]{12}"
             self.assertRegex(f.getvalue().strip(), id_regex)
 
+    def test_show(self):
+        """Test show"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show")
+            self.assertEqual("** class name missing **", f.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show FakeClass")
+            self.assertEqual("** instance id missing **", f.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel")
+            self.assertEqual("** instance id missing **", f.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel 1234")
+            self.assertEqual("** no instance found **", f.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            id_regex = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}"
+            "-[0-9a-f]{4}-[0-9a-f]{12}"
+            self.assertRegex(f.getvalue().strip(), id_regex)
+            HBNBCommand().onecmd("show BaseModel " + f.getvalue().strip())
+            self.assertRegex(f.getvalue().strip(), id_regex)
 
 if __name__ == "__main__":
     unittest.main()
