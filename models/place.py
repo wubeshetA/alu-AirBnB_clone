c0ae046e58e316638e1e5e2b65957981805016de
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-21 11:13:56 +0300

    Update Place model

--- models/place.py
+++ models/place.py
@@ -1,25 +1,77 @@
-#!/user/bin/python3
-"""place module for the AirBnB clone"""
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.review import Review
+from models.amenity import Amenity
 
-from models import BaseModel
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Place(BaseModel):
-    """Place class"""
+# create a table for the relationship between place and amenity
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(128),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
-    # class attributes
-    city_id = ""  # string - empty string: it will be the City.id
-    user_id = ""  # string - empty string: it will be the User.id
-    name = ""  # string - empty string
-    description = ""  # string - empty string
-    number_rooms = 0  # integer - 0
-    number_bathrooms = 0  # integer - 0
-    max_guest = 0  # integer - 0
-    price_by_night = 0  # integer - 0
-    latitude = 0.0  # float - 0.0
-    longitude = 0.0  # float - 0.0
-    amenity_ids = []  # list of string - empty list: it will be the list of
-    # Amenity.id later
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = "places"
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
+    # create amenity_ids list to store all Amenity.id linked to the Place
+    amenity_id = []
+
+    if storage_type == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
+
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+
+    else:
+        @property
+        def reviews(self):
+            """ getter attribute reviews that returns the list of Review
+            instances with place_id equals to the current Place.id """
+
+            review_list = []
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """ getter attribute amenities that returns the list of Amenity
+            instances based on the attribute amenity_ids that contains all
+            Amenity.id linked to the Place """
+
+            return self.amenity_ids
+
+        @amenities.setter
+        def set_amenity_ids(self, obj):
+            """ setter attribute amenities that handles append method for
+            adding an Amenity.id to the attribute amenity_ids """
+
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
