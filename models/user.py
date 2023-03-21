3e4d5584c1ec9c1dea70208ec5e1d34c6176effa
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-21 11:11:25 +0300

    Update User model

--- models/user.py
+++ models/user.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""user module for the AirBnB clone"""
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models import BaseModel
 
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-class User(BaseModel):
-    """User class"""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
