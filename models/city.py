dd73592e19a10d69a1cadf1e3f57b2aa42649814
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-21 09:57:54 +0300

    Update City to map it to a table

--- models/city.py
+++ models/city.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""city module for the AirBnB"""
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+# import sqlalchemy modules
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
-from models import BaseModel
 
-
-class City(BaseModel):
-    """City class"""
-
-    state_id = ""
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete', backref="cities")
